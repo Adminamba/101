7f340ad49416738ffb02055b4931c7a3298cc0cb
Adminamba <chechmanuel@gmail.com>
2026-04-25 20:20:50 +0700

    Refactor audio playback with playAudio function
    Refactored audio playback to use a dedicated function and multiple event triggers.

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-document.body.addEventListener("click", () => {
-  document.getElementById("horror").play();
-}, { once: true });
+const audio = document.getElementById("horror");
+
+function playAudio() {
+  audio.play().then(() => {
+    console.log("Audio jalan");
+  }).catch((err) => {
+    console.log("Error:", err);
+  });
+}
+
+// pakai banyak trigger biar pasti kena
+document.addEventListener("click", playAudio, { once: true });
+document.addEventListener("touchstart", playAudio, { once: true });
+document.addEventListener("scroll", playAudio, { once: true });
